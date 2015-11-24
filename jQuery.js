318c480f37ae606592ab7450fcd8b5a7c95e07f9
FinTheHuman <edward@Edwards-Air.home>
2015-11-23 20:56:45 -0500

    Color changing backround hurts my eyes so I got rid of it.

--- jQuery.js
+++ jQuery.js
@@ -3,22 +3,22 @@ $(document).ready(function(){
 		if(e.keyCode == 38){ //up
 			$('#test').stop().animate({marginTop:"-=100"});
 			$('#test').css({'background-color' : 'red'});
-			$('body').css({'background-color' : 'blue'});
+			//$('body').css({'background-color' : 'blue'});
 		}
 		if(e.keyCode == 39){ //right
 			$('#test').stop().animate({marginLeft:"+=100"});
 			$('#test').css({'background-color' : 'blue'});
-			$('body').css({'background-color' : 'red'});
+			//$('body').css({'background-color' : 'red'});
 		}
 		if(e.keyCode == 40){ //down
 			$('#test').stop().animate({marginTop:"+=100"});
 			$('#test').css({'background-color' : 'yellow'});
-			$('body').css({'background-color' : 'green'});
+			//$('body').css({'background-color' : 'green'});
 		}
 		if(e.keyCode == 37){ //left
 			$('#test').stop().animate({marginLeft:"-=100"});
 			$('#test').css({'background-color' : 'green'});
-			$('body').css({'background-color' : 'yellow'});
+			//$('body').css({'background-color' : 'yellow'});
 		}
 	});
 });
