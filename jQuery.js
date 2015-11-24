801a375ddf2d4157782c2e7788a6a88a4ebd55b1
FinTheHuman <edward@Edwards-MacBook-Air.local>
2015-11-24 13:00:23 -0500

    Flashing backgrounds are back

--- jQuery.js
+++ jQuery.js
@@ -3,24 +3,23 @@ $(document).ready(function(){
 		if(e.keyCode == 38){ //up
 			$('#test').stop().animate({marginTop:"-=100"});
 			$('#test').css({'background-color' : 'red'});
-			//$('body').css({'background-color' : 'blue'});
+			$('body').css({'background-color' : 'blue'});
 		}
 		if(e.keyCode == 39){ //right
 			$('#test').stop().animate({marginLeft:"+=100"});
 			$('#test').css({'background-color' : 'blue'});
-			//$('body').css({'background-color' : 'red'});
+			$('body').css({'background-color' : 'red'});
 		}
 		if(e.keyCode == 40){ //down
 			$('#test').stop().animate({marginTop:"+=100"});
 			$('#test').css({'background-color' : 'yellow'});
-			//$('body').css({'background-color' : 'green'});
+			$('body').css({'background-color' : 'green'});
 		}
 		if(e.keyCode == 37){ //left
 			$('#test').stop().animate({marginLeft:"-=100"});
 			$('#test').css({'background-color' : 'green'});
-			//$('body').css({'background-color' : 'yellow'});
+			$('body').css({'background-color' : 'yellow'});
 		}
 	});
 
-	//$('#test').draggable();
 });
