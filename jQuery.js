6dbc51c507926f01e296dce1bdb5e9d235f6cb17
FinTheHuman <edward@Edwards-Air.home>
2016-02-03 19:55:06 -0500

    Possibly a little game in the works

--- jQuery.js
+++ jQuery.js
@@ -1,24 +1,36 @@
 $(document).ready(function(){
+
+
+
+
+
+
+
+
+
+
+
+
 	$('body').keydown(function(e){
 		if(e.keyCode == 38){ //up
-			$('#test').stop().animate({marginTop:"-=100"});
 			$('#test').css({'background-color' : 'red'});
 			$('body').css({'background-color' : 'blue'});
+			//$('#number').append(1);
 		}
 		if(e.keyCode == 39){ //right
-			$('#test').stop().animate({marginLeft:"+=100"});
 			$('#test').css({'background-color' : 'blue'});
 			$('body').css({'background-color' : 'red'});
+			//$('#number').append(1);
 		}
 		if(e.keyCode == 40){ //down
-			$('#test').stop().animate({marginTop:"+=100"});
 			$('#test').css({'background-color' : 'yellow'});
 			$('body').css({'background-color' : 'green'});
+			//$('#number').append(1);
 		}
 		if(e.keyCode == 37){ //left
-			$('#test').stop().animate({marginLeft:"-=100"});
 			$('#test').css({'background-color' : 'green'});
 			$('body').css({'background-color' : 'yellow'});
+			//$('#number').append(1);
 		}
 	});
 	$('#test').draggable();
