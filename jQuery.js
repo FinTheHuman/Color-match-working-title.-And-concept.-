50dc1805763016160b84f2e1103c9668169486ba
FinTheHuman <edward@Edwards-Air.home>
2016-04-14 21:23:11 -0400

    A little set up for a color randomizer.

--- jQuery.js
+++ jQuery.js
@@ -15,17 +15,17 @@ var colors = ["rgba(255, 0, 0, 1)", "rgba(0, 255, 0, 1)", "rgba(0, 0, 255, 1)"];
 		}
 		if(e.keyCode == 39){ //right
 			$('#test').css({'background-color' : 'blue'});
-			$('body').css({'background-color' : 'red'});
+			$('body').css({'background-color' : colors[]});
 			//$('#number').append(1);
 		}
 		if(e.keyCode == 40){ //down
 			$('#test').css({'background-color' : 'yellow'});
-			$('body').css({'background-color' : 'green'});
+			$('body').css({'background-color' : colors[]});
 			//$('#number').append(1);
 		}
 		if(e.keyCode == 37){ //left
 			$('#test').css({'background-color' : 'green'});
-			$('body').css({'background-color' : 'yellow'});
+			$('body').css({'background-color' : colors[]});
 			//$('#number').append(1);
 		}
 	});
